e29113b8c7c7650c31418537ca1cd1ce5773da44
Jon Kalb <github@kalbweb.com>
2025-02-01 10:24:10 -0800

    Added Bitout_integral_value
    Removed Bitset_output_iter and Bitset_forward_iter in favor of bitset_iter with is a random access iterator that can be used for both input and output.
    
    Added Bitout_integral_value for writing the contents of a bitset into an integral value.
    
    Added some comments to the library.
    
    Significant clean up of whitespace style.
    
    In test file, added comment about the need for unit tests (which don't rely on visual inspection of output).
    
    Added "test" for Bitout_integral_value.

--- iterator_bitset_plus/main.t.cpp
+++ iterator_bitset_plus/main.t.cpp
@@ -11,7 +11,13 @@
 
 
 int main() {
-    std::cout<< "$$ testing biterator::Bitset_output_iter:\n\n";
+    /*
+        These "tests" are simple demonstrations of how to use the library and require visual
+        verification that results are as expected.
+        Real unit test coverage needs to be added.
+    */
+
+    std::cout<< "\n\n$$ testing biterator::bitset_iter:\n\n";
 
     std::vector<bool> binary_source{ true, true, false, false, true, true, false, false, true };  // size = 9
     std::string       string_source{ "testing testing" }; //012345678901234
@@ -20,12 +26,12 @@ int main() {
 
     std::cout << "$$ binary_source:";
     std::copy(begin(binary_source), end(binary_source), std::ostream_iterator<bool>(std::cout));
-    std::copy(begin(binary_source), end(binary_source), biterator::Bitset_iter{ small_sink });
+    std::copy(begin(binary_source), end(binary_source), biterator::bitset_iter{ small_sink });
     std::cout << "\n$$ small_sink   :" << small_sink;
-    std::copy(begin(binary_source) + 3, end(binary_source), biterator::Bitset_iter{ small_sink, 3 });
+    std::copy(begin(binary_source) + 3, end(binary_source), biterator::bitset_iter{ small_sink, 3 });
     std::cout << "\n$$ small_sink   :   " << small_sink.to_string().substr(3) << " (offset of 3)\n\n";
 
-    std::cout << "$$ testing biterator::Bitsource_forward_iter:";
+    std::cout << "$$ testing biterator::Bitsource_forward_iter:\n\n";
 
     std::cout << "$$ string_source:";
     std::for_each(begin(string_source), end(string_source),
@@ -36,12 +42,22 @@ int main() {
                 std::cout << ((mask & c)? "1": "0");
             }
         });
-    std::copy(biterator::begin<6, 0>(string_source), biterator::end<6, 0>(string_source), biterator::Bitset_output_iter{ big_sink });
+    std::copy(biterator::begin<6, 0>(string_source), biterator::end<6, 0>(string_source), biterator::bitset_iter{ big_sink });
     std::cout << "\n$$ big_sink     :" << big_sink;
     auto starting_position{biterator::begin<6, 0>(string_source)};
     std::advance(starting_position, 10);
-    std::copy(starting_position, biterator::end<6, 0>(string_source), biterator::Bitset_output_iter{ big_sink, 10 });
+    std::copy(starting_position, biterator::end<6, 0>(string_source), biterator::bitset_iter{ big_sink, 10 });
     std::cout << "\n$$ big_sink     :          " << big_sink.to_string().substr(10) << " (offset of 10)\n\n";
+
+    std::cout << "$$ testing biterator::Bitout_integral_value:\n\n";
+
+    int32_t int_value{0};
+    for (int i{0}; i < 15; ++i)
+    {
+        std::copy(biterator::bitset_iter{big_sink} + (i * 7), biterator::bitset_iter{big_sink} + ((i + 1) * 7), biterator::begin(int_value));
+        std::cout << int_value << '(' << char(int_value) << ") ";
+        int_value = 0;
+    }
 
     std::cout << "\n###" << std::endl;
 }
